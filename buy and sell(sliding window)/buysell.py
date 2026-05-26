fd12071561e527d5470b4600af24ca433d1121c2
Lakshmiprasanna <prasannalakshmi6214@gmail.com>
2026-05-26 16:32:52 +0530

    intial commit

---
+++ buy and sell(sliding window)/buysell.py
@@ -0,0 +1,14 @@
+def maxProfit(prices):
+        left=0
+        right=1
+        max_profit=0
+        while right<len(prices):
+            if prices[right]>prices[left]:
+                profit= prices[right]-prices[left]
+                max_profit=max(profit, max_profit)
+            else:
+                left = right
+            right +=1
+        return max_profit
+n = list(map(int,input("PRICES:").split()))
+print(maxProfit(n))
